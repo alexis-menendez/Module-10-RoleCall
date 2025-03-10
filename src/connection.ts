d6de8df5c24629570a21d74ae935944b7d92a7c5
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-09 21:45:59 -0500

    update

--- src/connection.ts
+++ src/connection.ts
@@ -1,3 +1,10 @@
 //File path: rolecall/src/connection.ts
 
 
+// import necessary modules
+import colors from 'colors';
+
+// laod environmental variables 
+import dotenv from 'dotenv';
+dotenv.config();
+
