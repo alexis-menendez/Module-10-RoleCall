afd13ced8afa91d8d6c1b8c5d7f75754ce150e6d
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-09 21:31:17 -0500

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -18,8 +18,8 @@ CREATE TABLE department (
 -- Create a table to store role/job information
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
@@ -29,7 +29,7 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
