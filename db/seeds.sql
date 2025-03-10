74bdba4315d5293fe0aff0d3793cb589fab3fcba
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-09 21:42:40 -0500

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,12 @@
 -- File path: rolecall/db/seeds.sql
 
+-- Insert departments into the department table
+INSERT INTO department (name)
+VALUES ('Moss & Mushroom Storytelling (Script Writing Department)'),
+       ('Twinkle & Twig Animators (Animation Department)'),
+       ('Whimsy Acoustics & Enchantment Soundscapes (Sound Design Department)'),
+       ('Rainbow Beam Projection (Screening Department)'),
+       ('Harmonious Roots (Voice Acting Department)'),
+       ('Gnome-Unicorn Relations (Public Relations Department)'),
+       ('Gilded Toadstool Council (Management Department)'),
+       ('Hollow Oak Compliance Keepers (Human Resources Department)');
