ba94fba6a77982094cdd730a4d453d04202e5c57
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-09 21:46:42 -0500

    update

--- src/connection.ts
+++ src/connection.ts
@@ -12,3 +12,11 @@ dotenv.config();
 import pg from 'pg';
 const { Pool } = pg;
 
+// create a postgreSQL connection pool using environmental variables
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    port: 5432,
+  });
