5ac407fcb46f5f35fb8fa3f48c2cadbcd187a477
Alexis Menendez <alexis.menendez@austincc.edu>
2025-03-11 19:11:38 -0500

    completed TODO's for connections

--- src/connection.ts
+++ src/connection.ts
@@ -1,19 +1,36 @@
 //File path: rolecall/src/connection.ts
 
 
-// TODO: import necessary modules
+// import necessary modules
+import colors from 'colors';
 
+// laod environmental variables 
+import dotenv from 'dotenv';
+dotenv.config();
 
-// TODO: laod environmental variables 
+// import PostgreSQL client for database connection & destructure Pool class from pg package
+import pg from 'pg';
+const { Pool } = pg;
 
+// create a postgreSQL connection pool using environmental variables
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: process.env.DB_NAME,
+    port: 5432,
+  });
 
-// TODO: import PostgreSQL client for database connection & destructure Pool class from pg package
+  // connect to the database and handle errors
+const connectToDb = async () => {
+    try {
+      await pool.connect();
+      console.log(colors.rainbow('✅ Success: Connected to the database.'));
+    } catch (err) {
+      console.error(colors.red('❌ Error: Could not connect to the database.'), err);
+      process.exit(1);
+    }
+  };
 
-
-// TODO: create a postgreSQL connection pool using environmental variables
-
-
-  // TODO: connect to the database and handle errors
-
-
-  // TODO: export the database pool and connection function for use in other modules
+  // export the database pool and connection function for use in other modules
+export { pool, connectToDb };
